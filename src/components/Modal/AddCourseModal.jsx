bcaf094cd0842cce57e1ff8df7a925ac78599f2c
Rupom-Raz <rupomk70@gmail.com>
2022-07-19 10:02:39 +0600

    formik updated

--- src/components/Modal/AddCourseModal.jsx
+++ src/components/Modal/AddCourseModal.jsx
@@ -24,43 +24,47 @@ const style = {
     boxShadow: 24,
     p: 4,
 };
+const validationSchema = Yup.object().shape({
+    courseTitle: Yup.string().required("Course title is required"),
+    courseCode: Yup.string()
+        .max(10, "Must be 10 characters or less")
+        .required("Course code is required"),
+    courseHours: Yup.number()
+        .max(5, "No longer than 5 hours")
+        .min(2, "Must be longer than 2 hours")
+        .required("Course hours is required"),
+    credit: Yup.number()
+        .max(3, "No longer than 3 credits")
+        .min(1, "Must be longer than 1 credit")
+        .required("Course credit is required"),
+    program: Yup.string().required("Program is required").nullable(),
+});
+
+const initialValues = {
+    courseTitle: "",
+    courseCode: "",
+    courseHours: "",
+    credit: "",
+    program: "",
+};
+
+const onSubmit = (values, submitProps) => {
+    console.log("Form Data", values);
+    console.log("Submit props", submitProps);
+    submitProps.setSubmitting(false);
+    submitProps.resetForm();
+};
 
 const AddCourseModal = ({ handleClose, open }) => {
-    const validate = Yup.object({
-        courseTitle: Yup.string().required("Course title is required"),
-        courseCode: Yup.string()
-            .max(10, "Must be 10 characters or less")
-            .required("Course code is required"),
-        courseHours: Yup.number()
-            .max(5, "No longer than 5 hours")
-            .min(2, "Must be longer than 2 hours")
-            .required("Course hours is required"),
-        credit: Yup.number()
-            .max(3, "No longer than 3 credits")
-            .min(1, "Must be longer than 1 credit")
-            .required("Course credit is required"),
-        program: Yup.string().required("Program is required"),
-    });
+    const [formValues, setFormValues] = React.useState(null);
 
-    const [program, setProgram] = React.useState("");
-
-    const handleChange = (event) => {
-        setProgram(event.target.value);
-    };
     return (
         <>
             <Formik
-                validationSchema={validate}
-                initialValues={{
-                    courseTitle: "",
-                    courseCode: "",
-                    courseHours: "",
-                    credit: "",
-                    program: "",
-                }}
-                onSubmit={(values) => {
-                    console.log(values);
-                }}
+                initialValues={formValues || initialValues}
+                validationSchema={validationSchema}
+                onSubmit={onSubmit}
+                enableReinitialize
             >
                 {(formik) => (
                     <Form>
@@ -79,21 +83,29 @@ const AddCourseModal = ({ handleClose, open }) => {
                                     Add Courses
                                 </Typography>
                                 <TextInput
+                                    value={formik.values.courseTitle}
+                                    onChange={formik.handleChange}
                                     name="courseTitle"
                                     label="Course Title"
                                     type="text"
                                 />
                                 <TextInput
+                                    value={formik.values.courseCode}
+                                    onChange={formik.handleChange}
                                     name="courseCode"
                                     label="Course Code"
                                     type="text"
                                 />
                                 <TextInput
+                                    value={formik.values.credit}
+                                    onChange={formik.handleChange}
                                     name="credit"
                                     label="Course Credit"
                                     type="number"
                                 />
                                 <TextInput
+                                    value={formik.values.courseHours}
+                                    onChange={formik.handleChange}
                                     name="courseHours"
                                     label="Course Hours"
                                     type="number"
@@ -103,19 +115,23 @@ const AddCourseModal = ({ handleClose, open }) => {
                                     Select Programs
                                 </InputLabel>
                                 <Select
+                                    name="program"
                                     fullWidth
                                     variant="standard"
                                     labelId="demo-simple-select-label"
                                     id="demo-simple-select"
-                                    value={program}
+                                    value={formik.values.program}
                                     label="Select Programs"
-                                    onChange={handleChange}
+                                    onChange={formik.handleChange}
                                 >
                                     <MenuItem value="B.Sc">B.Sc</MenuItem>
                                     <MenuItem value="M.Sc">M.Sc</MenuItem>
                                 </Select>
                                 <Button
                                     type="submit"
+                                    disabled={
+                                        !formik.isValid || formik.isSubmitting
+                                    }
                                     variant="outlined"
                                     style={{
                                         margin: "10px 0px",
